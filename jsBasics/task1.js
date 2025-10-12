5f0bf4e719f477f2ef43b5c9bcb8a922fb523fd2
o-bakhmatska <bahmatskaya.o@gmail.com>
2025-10-12 22:56:41 +0300

    Homework lesson 4

---
+++ jsBasics/task1.js
@@ -0,0 +1,10 @@
+//Створіть програму, в якій ви оголосите змінні для кожного з примітивних типів даних:
+//рядок, число, буль, null та undefined. 
+//Присвойте їм значення та виведіть кожну змінну на консоль.
+
+const myName = "Oksana";
+const age = 30;
+const havePet = true;
+const car = undefined;
+
+console.log(myName, age, havePet, car)
