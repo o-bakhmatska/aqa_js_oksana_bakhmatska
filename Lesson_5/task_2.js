9208358f6cbd87f64ff5ec2521a4f7252e8e6f7c
o-bakhmatska <bahmatskaya.o@gmail.com>
2025-10-18 23:06:07 +0300

    Homework  5

---
+++ Lesson_5/task_2.js
@@ -0,0 +1,20 @@
+//Виконайте попереднє завдання але використовуючи конструкцію switch/case
+
+const averageGrade = 100;
+
+switch (true) {
+  case averageGrade < 60:
+    console.log("Незадовільно");
+    break;
+  case averageGrade >= 60 && averageGrade <= 70:
+    console.log("Задовільно");
+    break;
+  case averageGrade >= 71 && averageGrade <= 80:
+    console.log("Добре");
+    break;
+  case averageGrade >= 81 && averageGrade <= 90:
+    console.log("Дуже добре");
+    break;
+  default:
+    console.log("Відмінно");
+}
