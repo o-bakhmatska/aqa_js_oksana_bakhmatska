93f9f2a40c1473c0fd35964714073a1ef8275a8f
Oksana Bakhmatska <118731761+o-bakhmatska@users.noreply.github.com>
2025-10-27 19:24:16 +0200

    Homework 8 (#8)

---
+++ Lesson_8/task_6.js
@@ -0,0 +1,10 @@
+// У вас є вихідний масив сonst numbersList = [1,10,14,2,4,5,43,34]
+// Створіть новий масив на основі вихідного масиву (копіюйте його)
+// Відсортуйте створений масив (від меншого до більшого)
+// Виведіть обидва масиви в консоль
+
+const numbersList = [1,10,14,2,4,5,43,34];
+const sortedNumbers = [...numbersList];
+sortedNumbers.sort((a, b) => a - b);
+console.log("Original array:", numbersList);
+console.log("Sorted array:", sortedNumbers);
