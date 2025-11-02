80ebb0a5b55fd49bcc6f38c9bf45ee72f6b4a87d
o-bakhmatska <bahmatskaya.o@gmail.com>
2025-11-02 21:18:01 +0200

    Homework 9

---
+++ Lesson_9/task1.js
@@ -0,0 +1,17 @@
+// Створіть порожній об'єкт з ім'ям student.
+// Додайте до об'єкта student наступні властивості:
+// firstName: рядок, представляє ім'я студента.
+// lastName: рядок, представляє прізвище студента.
+// age: число, представляє вік студента.
+// courses: масив, представляє список курсів, які він вивчає (наприклад, ["Математика", "Історія", "Програмування"]).
+// Виведіть об'єкт student в консоль.
+
+
+let student = {};
+student.firstName = "Alex";
+student.lastName = "Forbes";
+student.age = 20;
+student.courses = ["Math", "Quality Assurance", "Programming"];
+
+console.log(student);
+
---
+++ Lesson_9/task4.js
@@ -0,0 +1,19 @@
+// Створіть об'єкт person з такими властивостями:
+// firstName: рядок, представляє ім'я особи.
+// lastName: рядок, представляє прізвище особи.
+// age: число, представляє вік особи.
+// Додайте до об'єкта person властивість email, яка буде містити електронну пошту особи.
+// Видаліть властивість age з об'єкта person.
+// Виведіть оновлений об'єкт person в консоль.
+
+const person = {
+    firstName: "Maria",
+    lastName: "Berger",
+    age: 25
+  };
+
+person.email = "maria.berger@example.com";
+console.log("Person object with email:", person);
+
+delete person.age;
+console.log("Person object without age:", person);
