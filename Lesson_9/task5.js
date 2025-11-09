50be2b110038e3d111635cdf1db5f3fec7329fd0
Oksana Bakhmatska <118731761+o-bakhmatska@users.noreply.github.com>
2025-11-09 21:01:11 +0200

    Homework 9 (#10)
    * Homework 9
    
    * Homework 9 (updated)

---
+++ Lesson_9/task5.js
@@ -0,0 +1,13 @@
+// Створіть масив об'єктів users де обєкти мають довільні властивості (наприклад, name, email, age, тощо).
+// Використовуючи цикл for...of, переберіть всі елементи масиву та виведіть їхні значення в консоль.
+// Зробіть деструктуризацію в циклі
+
+const users = [
+    {name: "Oksana", email: "oksana@example.com", age: 30},
+    {email: "test@test.com"},
+    {name: "Oleg", age: 36},
+    {}
+]
+for (const {name, email, age} of users){
+    console.log(`${name ?? "-----"}, ${email ?? "-----"}, ${age ?? "--"} `);
+}
